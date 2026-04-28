f3a36dacb31b18714fb2f325c24f7f826ed8fd5c
Ardyansyah30 <ardyansyahm492@gmail.com>
2026-04-28 09:46:17 +0700

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://portofolio-midwfie.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://portofolio-midwfie.vercel.app/tarif",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://portofolio-midwfie.vercel.app/sertifikat",
+      lastModified: new Date(),
+    },
+  ];
+}
